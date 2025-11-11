157dd9d4daba1b8bb559881409b8ab6464d2e86d
Jiafei Pan <Jiafei.Pan@nxp.com>
2025-11-11 15:46:08 +0800

    net: libvirtual_switch: adpat to mcux-sdk NG
    Update CMake to adapt to SDK-NG.
    
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>

--- net/boards/evkmimxrt1180_cm33/mcux/mcux_virtual_switch.cmake
+++ net/boards/evkmimxrt1180_cm33/mcux/mcux_virtual_switch.cmake
@@ -14,9 +14,12 @@ if(CONFIG_MCUX_PRJSEG_module.hmc.virtual_sw_lib)
         net
     )
 
+    set(VirtualSwitchBuildPath "${APPLICATION_BINARY_DIR}/virtual_switch_build")
+    add_subdirectory("${CMAKE_CURRENT_LIST_DIR}/../" "${VirtualSwitchBuildPath}/")
     mcux_add_library(
-        BASE_PATH ${SdkRootDirPath}/gen_avb-libs
+        BASE_PATH ${VirtualSwitchBuildPath}
         LIBS "libvirtual-switch.a"
         TOOLCHAINS armgcc iar mcux
     )
+
 endif()
