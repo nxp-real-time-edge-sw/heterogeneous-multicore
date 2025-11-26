0515f292cfbaf1d289be53e243b92a2dd6d9d93e
Jiafei Pan <Jiafei.Pan@nxp.com>
2025-11-26 17:02:38 +0800

    dsa: virtual_switch: remove source and include
    It will be taken care in genavb.
    
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>

--- net/boards/evkmimxrt1180/cm33/mcux/mcux_virtual_switch.cmake
+++ net/boards/evkmimxrt1180/cm33/mcux/mcux_virtual_switch.cmake
@@ -1,19 +1,4 @@
 if(CONFIG_MCUX_PRJSEG_module.hmc.virtual_sw_lib)
-    mcux_add_source(
-        BASE_PATH ${SdkRootDirPath}/middleware/heterogeneous-multicore
-        SOURCES
-        include/net/*.h
-        net/lib_port_genavb.h
-        net/lib_virtual_switch.h
-    )
-
-    mcux_add_include(
-        BASE_PATH ${SdkRootDirPath}/middleware/heterogeneous-multicore
-        INCLUDES
-        include/net
-        net
-    )
-
     set(VirtualSwitchBuildPath "${APPLICATION_BINARY_DIR}/virtual_switch_build")
     add_subdirectory("${CMAKE_CURRENT_LIST_DIR}/../" "${VirtualSwitchBuildPath}/")
     mcux_add_library(
@@ -21,5 +6,4 @@ if(CONFIG_MCUX_PRJSEG_module.hmc.virtual_sw_lib)
         LIBS "libvirtual-switch.a"
         TOOLCHAINS armgcc iar mcux
     )
-
 endif()
