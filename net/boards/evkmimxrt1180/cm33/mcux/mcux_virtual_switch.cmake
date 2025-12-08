20fff5054c15be15a6f7e4053312e483f27223bf
Tobias Aguiar <tobias.aguiarsilvadealmeida@nxp.com>
2025-12-08 13:35:24 +0100

    dsa: add back include path for virtual_switch library
    Add back virtual_switch library include paths. Fixes DSA NG builds.
    
    Signed-off-by: Tobias Aguiar <tobias.aguiarsilvadealmeida@nxp.com>

--- net/boards/evkmimxrt1180/cm33/mcux/mcux_virtual_switch.cmake
+++ net/boards/evkmimxrt1180/cm33/mcux/mcux_virtual_switch.cmake
@@ -1,4 +1,12 @@
 if(CONFIG_MCUX_PRJSEG_module.hmc.virtual_sw_lib)
+    mcux_add_include(
+        BASE_PATH ${SdkRootDirPath}/middleware/heterogeneous-multicore
+        INCLUDES
+        include/net
+        net
+        TOOLCHAINS armgcc
+    )
+
     set(VirtualSwitchBuildPath "${APPLICATION_BINARY_DIR}/virtual_switch_build")
     add_subdirectory("${CMAKE_CURRENT_LIST_DIR}/../" "${VirtualSwitchBuildPath}/")
     mcux_add_library(
