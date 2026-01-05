ec0a58f69227866da7f417f49e637e9e10447804
Hou Zhiqiang <Zhiqiang.Hou@nxp.com>
2026-01-05 17:27:11 +0800

    os: freertos: add imx943evk M-core support
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

---
+++ os/freertos/Core_M/boards/imx943evk/hal_config.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2026 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#define SM_CLOCK        1
+#define CCM_CLOCK       0
+#define SM_PINCTRL      1
+#define IOMUXC_PINCTRL  0
+#define SM_POWER        1
+#define CCMSRCGPC_POWER 0
+#define SM_RESET        1
+#define SRC_RESET       0
